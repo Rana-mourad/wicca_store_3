3876fc4213fabdf1105fea19833da7f7798c321b
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-06 06:49:15 +0200

    add new page

---
+++ lib/views/product_details_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatelessWidget {
+  final String name;
+  final double price;
+  final String description;
+  final String imageUrl;
+
+  ProductDetailsPage({
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              imageUrl,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '\$${price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 175, 94, 76),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'Description:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    description,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
