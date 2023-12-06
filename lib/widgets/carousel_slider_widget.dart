d9c67329d0c9db2a0c955f0d8d24384be68806a5
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-06 06:49:59 +0200

    modfy carousel

--- lib/widgets/carousel_slider_widget.dart
+++ lib/widgets/carousel_slider_widget.dart
@@ -1,88 +1,103 @@
-import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:dots_indicator/dots_indicator.dart';
-import 'package:wicca_store_3/models/ads.model.dart';
-import 'package:wicca_store_3/widgets/home/ads.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:wicca_store_3/widgets/cutom_button.dart';
 
 class CarouselSliderEx extends StatefulWidget {
-  final List<Ad> ads;
-  const CarouselSliderEx({required this.ads, Key? key}) : super(key: key);
+  const CarouselSliderEx(
+      {required this.imageUrls, required this.onBtnPressed, super.key});
+
+  final List<String> imageUrls;
+  final void Function() onBtnPressed;
 
   @override
   State<CarouselSliderEx> createState() => _CarouselSliderExState();
 }
 
 class _CarouselSliderExState extends State<CarouselSliderEx> {
-  int currentPosition = 0;
-
-  CarouselOptions get options => CarouselOptions(
-        onPageChanged: (index, _) {
-          currentPosition = index;
-          setState(() {});
-        },
-        height: 150,
-        aspectRatio: 16 / 9,
-        viewportFraction: 0.8,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        reverse: false,
-        autoPlay: true,
-        autoPlayInterval: const Duration(seconds: 3),
-        autoPlayAnimationDuration: const Duration(milliseconds: 800),
-        autoPlayCurve: Curves.fastOutSlowIn,
-        enlargeCenterPage: true,
-        enlargeFactor: 0.3,
-        scrollDirection: Axis.horizontal,
-      );
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        CarouselSlider(
-          options: options,
-          items: widget.ads.map((ad) {
-            return Builder(
-              builder: (BuildContext context) {
-                return GestureDetector(
-                  onTap: () {
-                    // Navigate to AdsPage when the carousel item is tapped
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AdsPage(ad: ad),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 90, 7),
-                    ),
-                    child: Text(
-                      ad.title ?? 'No Title',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ),
-                );
+        CarouselSlider.builder(
+            itemCount: widget.imageUrls.length,
+            itemBuilder: (BuildContext context, int index, int pageViewIndex) =>
+                buildSliderImage(index),
+            options: CarouselOptions(
+              height: 200,
+              viewportFraction: .9,
+              padEnds: false,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              enlargeFactor: 0.15,
+              onPageChanged: (index, _) {
+                index = index;
+                setState(() {});
               },
-            );
-          }).toList(),
+              scrollDirection: Axis.horizontal,
+            )),
+        const SizedBox(
+          height: 5,
         ),
-        DotsIndicator(
-          dotsCount: widget.ads.length,
-          position: currentPosition,
-          decorator: DotsDecorator(
-            size: const Size.square(9.0),
-            activeSize: const Size(18.0, 9.0),
-            activeShape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5.0),
-            ),
+        AnimatedSmoothIndicator(
+          activeIndex: index,
+          count: widget.imageUrls.length,
+          effect: const ExpandingDotsEffect(
+            activeDotColor: Color.fromARGB(255, 255, 51, 0),
           ),
         )
       ],
+    );
+  }
+
+  Widget buildSliderImage(int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Stack(
+          children: [
+            Image.network(
+              widget.imageUrls[index],
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const Positioned(
+                left: 20,
+                top: 40,
+                child: Text(
+                  'New Offers',
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                  // maxLines: 3,
+                  // textDirection: TextDirection.rtl,
+                  //  textAlign: TextAlign.justify,  overflow: TextOverflow.clip,
+                )),
+            Positioned(
+              left: 20,
+              bottom: 20,
+              child: CustomButton(
+                text: "SEE MORE",
+                onBtnPressed: widget.onBtnPressed,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
