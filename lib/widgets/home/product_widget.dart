1a064ddb2d62e6608fd52d1d320cf3c18c52df12
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-13 10:39:37 +0200

    modfy product widget

--- lib/widgets/home/product_widget.dart
+++ lib/widgets/home/product_widget.dart
@@ -1,71 +1,113 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wicca_store_3/models/product.model.dart';
+import 'package:wicca_store_3/provider/FavouriteProvider.dart';
 import 'package:wicca_store_3/views/product_details_page.dart';
 
 class ProductWidget extends StatelessWidget {
-  final String name;
-  final double price;
-  final String description;
-  final String imageUrl;
-
-  ProductWidget({
-    required this.name,
-    required this.price,
-    required this.description,
-    required this.imageUrl,
-  });
+  final Product product;
+  const ProductWidget({required this.product, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
+    return InkWell(
+      onTap: () => Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => ProductDetailsPage(
-              name: name,
-              price: price,
-              description: description,
-              imageUrl: imageUrl,
-            ),
-          ),
-        );
-      },
-      child: Card(
-        // Your product widget UI here
-        child: Column(
-          children: [
-            Image.network(
-              imageUrl,
-              height: 100,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              builder: (_) => ProductDetailsPage(product: product))),
+      child: Container(
+        height: 220,
+        width: 170,
+        decoration: BoxDecoration(
+            color: const Color(0xffD9D9D9).withOpacity(.25),
+            borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 12, right: 12, bottom: 12, top: 8),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    name,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        'sale',
+                        style: TextStyle(fontSize: 12),
+                      ),
                     ),
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    '\$${price.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Color.fromARGB(255, 175, 94, 76),
-                    ),
-                  ),
+                  InkWell(
+                      onTap: () => onFavouriteClicked(context),
+                      child: Provider.of<FavouriteProvider>(
+                        context,
+                      ).isFavourite(product.id ?? 0)
+                          ? Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            )
+                          : Icon(
+                              Icons.favorite_outline,
+                            ))
                 ],
               ),
-            ),
-          ],
+              Expanded(
+                child: CachedNetworkImage(
+                  imageUrl: product.image ?? '',
+                  placeholder: (context, url) => const SizedBox(
+                      height: 15,
+                      width: 15,
+                      child: FittedBox(child: CircularProgressIndicator())),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    product.title ?? 'No Title',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: Colors.black.withOpacity(.6)),
+                  )),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    '${product.price}\$',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                        color: Colors.black.withOpacity(.75)),
+                  )),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void onFavouriteClicked(BuildContext context) {
+    if (Provider.of<FavouriteProvider>(context, listen: false)
+        .isFavourite(product.id ?? 0)) {
+      Provider.of<FavouriteProvider>(context, listen: false)
+          .removeProductFromFavourites(product.id ?? 0);
+      return;
+    }
+
+    Provider.of<FavouriteProvider>(context, listen: false)
+        .addProductToFavourites(product);
   }
 }
