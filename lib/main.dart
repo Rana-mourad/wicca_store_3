d276f2d14a6dc7959f0abaf253b43a034aaf2607
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-12-13 11:39:26 +0200

    add orofile provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,30 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wicca_store_3/provider/CartProvider.dart';
 import 'package:wicca_store_3/provider/FavouriteProvider.dart';
 import 'package:wicca_store_3/provider/ProductProvider.dart';
+import 'package:wicca_store_3/provider/ProfileProvider.dart';
 import 'package:wicca_store_3/theme/themeutils.dart';
-import 'package:wicca_store_3/views/splashpage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:wicca_store_3/views/login.dart';
+import 'package:wicca_store_3/views/master_page.dart';
+import 'package:wicca_store_3/views/splashpage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   var preferenceInstance = await SharedPreferences.getInstance();
   GetIt.I.registerSingleton<SharedPreferences>(preferenceInstance);
   var result = GetIt.I.allReadySync();
 
   if (result == true) {
-    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>> prefrences set successfully');
+    print('Preferences set successfully');
   } else {
-    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Error When Set prefrences');
+    print('Error when setting preferences');
   }
 
   GetIt.I.registerSingleton<ProductProvider>(ProductProvider());
   GetIt.I.registerSingleton<FavouriteProvider>(FavouriteProvider());
   GetIt.I.registerSingleton<CartProvider>(CartProvider());
+  GetIt.I.registerSingleton<ProfileProvider>(ProfileProvider());
 
   runApp(const MyApp());
 }
@@ -29,7 +34,7 @@ void main() async {
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +44,10 @@ class MyApp extends StatelessWidget {
       title: 'Wicca shop',
       theme: ThemeUtils.themeData,
       home: SplashPage(),
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/master": (context) => MasterPage()
+      },
     );
   }
 }
